b469b1fdd56cf715d5c6c9bc23022f625ff11b3c
Gagandeep Singh <gsin1653@my.centennialcollege.ca>
2021-02-13 18:17:15 +0530

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
+
+let contactFirstName = null;
+let contactLastName = null;
+let contactNumber = null;
+let contactEmail = null;
+let contactMessage = null;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+router.get('/', function (req, res, next) {
+  res.render('pages/home', {
+    title: 'Home', 
+    contactFirstName: contactFirstName,
+    contactLastName:contactLastName,
+    contactNumber: contactNumber,
+    contactEmail: contactEmail,
+    contactMessage: contactMessage
+  });
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+router.get('/home', function (req, res, next) {
+  res.render('pages/home', {
+    title: 'Home',
+    contactFirstName: contactFirstName,
+    contactLastName:contactLastName,
+    contactNumber: contactNumber,
+    contactEmail: contactEmail,
+    contactMessage: contactMessage
+  });
 });
 
 /* GET About Us page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+router.get('/about', function (req, res, next) {
+  res.render('pages/about', { title: 'About' });
 });
 
 /* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+router.get('/project', function (req, res, next) {
+  res.render('pages/projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+router.get('/services', function (req, res, next) {
+  res.render('pages/services', { title: 'Services' });
 });
 
 /* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+router.get('/contact', function (req, res, next) {
+  res.render('pages/contact', { title: 'Contact' });
+});
+
+/* POST Contact Us page. */
+router.post('/contact', function (req, res, next) {
+  contactFirstName = req.body.userFirstName;
+  contactLastName = req.body.userLastName;
+  contactNumber = req.body.userContactNumber;
+  contactEmail = req.body.userEmailId;
+  contactMessage = req.body.userMessage;
+  res.redirect('/home');
 });
 
 module.exports = router;
